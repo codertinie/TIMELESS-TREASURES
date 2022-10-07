8663b05c4665548307d06f280c50ccf71c44b150
codertinie <martin.munyao@moringaschool.com>
2022-10-07 18:30:22 +0300

    Added a custom hook to help in switching between pages

--- src/PICASPP/Screens/TopNav.js
+++ src/PICASPP/Screens/TopNav.js
@@ -3,20 +3,22 @@ import { useLocation, useNavigate } from "react-router-dom";
 const TopNav = () => {
   //location will help us get the current location
   const location = useLocation();
+  // declaring the navigation hook
+  const navigate = useNavigate()
   console.log(location)
 
   return (
   <div className=" flex gap-x-10">
-    <div className={`cursor-pointer 
+    <div onClick={() => navigate("")} className={`cursor-pointer 
     ${location.pathname === "/" ? " bg-orange-500" : ""}`}
     >HOME</div>
-    <div className={`cursor-pointer 
+    <div onClick={() => navigate("/land")} className={`cursor-pointer 
     ${location.pathname === "/land" ? " bg-green-700" : ""}`}>LAND</div>
-    <div className={`cursor-pointer 
+    <div onClick={() => navigate("/sea")} className={`cursor-pointer 
     ${location.pathname === "/sea" ? " bg-blue-500" : ""}`}>SEA</div>
-    <div className={`cursor-pointer 
+    <div onClick={() => navigate("/sky")} className={`cursor-pointer 
     ${location.pathname === "/sky" ? " bg-blue-800" : ""}`}>SKY</div>
-    <div className=" cursor-pointer">REGISTER</div>
+    <div onClick={() => navigate("/sign-up")} className=" cursor-pointer">REGISTER</div>
   </div>
   )
 }
