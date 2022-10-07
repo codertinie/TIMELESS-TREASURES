6c5bfebd98218acf304ad17328c255e424ca15d4
codertinie <martin.munyao@moringaschool.com>
2022-10-07 18:17:00 +0300

    Added background colors to topNav to help distinguish pages

--- src/PICASPP/Screens/TopNav.js
+++ src/PICASPP/Screens/TopNav.js
@@ -5,7 +5,20 @@ const TopNav = () => {
   const location = useLocation();
   console.log(location)
 
-  return null
+  return (
+  <div className=" flex gap-x-10">
+    <div className={`cursor-pointer 
+    ${location.pathname === "/" ? " bg-orange-500" : ""}`}
+    >HOME</div>
+    <div className={`cursor-pointer 
+    ${location.pathname === "/land" ? " bg-green-700" : ""}`}>LAND</div>
+    <div className={`cursor-pointer 
+    ${location.pathname === "/sea" ? " bg-blue-500" : ""}`}>SEA</div>
+    <div className={`cursor-pointer 
+    ${location.pathname === "/sky" ? " bg-blue-800" : ""}`}>SKY</div>
+    <div className=" cursor-pointer">REGISTER</div>
+  </div>
+  )
 }
 
 export default TopNav;
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import PICASAPP from './PICASPP';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <div>TIMELESS TREASURES</div>
     <PICASAPP />
   </React.StrictMode>
 );
