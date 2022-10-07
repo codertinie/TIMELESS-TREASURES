f4ad6bb803331d988f4f3dc84fd3e09d9d53372d
codertinie <martin.munyao@moringaschool.com>
2022-10-07 17:06:03 +0300

    Added an outlet hook

---
+++ src/PICASPP/Screens/index.js
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom";
+
+function Screens(){
+    return(
+        <div>
+            <div>TIMELESS TREASURES</div>
+            <Outlet />
+        </div>
+    )
+}
+
+export default Screens;
---
+++ src/PICASPP/index.js
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+
+import Home from "./Screens/Home";
+import Land from "./Screens/Land";
+import Sea from "./Screens/Sea";
+import Sky from "./Screens/Sky";
+import SinglePic from "./Screens/Singlepic";
+
+
+const PICASAPP=()=>{
+
+}
+
+export default PICASAPP;
