ba3a1ac656f8cb113b0c2c8684fc0561198423f0
codertinie <martin.munyao@moringaschool.com>
2022-10-08 20:29:07 +0300

    Added pictures to the home component

--- src/PICASPP/Screens/Home/index.js
+++ src/PICASPP/Screens/Home/index.js
@@ -1,6 +1,28 @@
-const Home=()=>{
-  return <div> Hey I am the Home Page</div>
+import { data } from "autoprefixer";
+import { useEffect, useState } from "react";
 
-};
+
+const Home = () => {
+  const [pictures, setPictures] = useState([])
+  useEffect(() => {
+    fetch("http://localhost:8001/pictures")
+      .then(resp => resp.json())
+      .then(setPictures)
+    console.log(pictures)
+  }, [])
+
+
+
+
+  return (
+    <div className=" flex flex-row flex-wrap gap-x-4 gap-y-3 place-content-center">
+      {pictures.map((pic, id) => {
+        return <div key={id} className=" rounded-lg flex flex-col shadow-lg">
+          <img className="w-96" src={pic.url} />
+        </div>
+      })}
+    </div>
+  )
+}
 
 export default Home;
--- src/PICASPP/Screens/TopNav.js
+++ src/PICASPP/Screens/TopNav.js
@@ -5,21 +5,21 @@ const TopNav = () => {
   const location = useLocation();
   // declaring the navigation hook, a custom hook
   const navigate = useNavigate()
-  console.log(location)
+  // console.log(location)
 
   return (
   <div className=" flex  flex-row gap-x-32 justify-evenly bg-slate-500 mx-3 my-7 text-2xl ">
-    <div onClick={() => navigate("")} className={`hover:underline decoration-4 cursor-pointer 
+    <div onClick={() => navigate("")} className={`hover:underline flex decoration-4 cursor-pointer 
     ${location.pathname === "/" ? " bg-orange-500" : ""}`}
     >HOME</div>
-    <div onClick={() => navigate("/land")} className={`hover:underline cursor-pointer 
+    <div onClick={() => navigate("/land")} className={`hover:underline flex cursor-pointer 
     ${location.pathname === "/land" ? " bg-green-700" : ""}`}>LAND</div>
-    <div onClick={() => navigate("/sea")} className={`hover:underline cursor-pointer 
+    <div onClick={() => navigate("/sea")} className={`hover:underline flex cursor-pointer 
     ${location.pathname === "/sea" ? " bg-blue-500" : ""}`}>SEA</div>
-    <div onClick={() => navigate("/sky")} className={`hover:underline cursor-pointer 
+    <div onClick={() => navigate("/sky")} className={`hover:underline flex cursor-pointer 
     ${location.pathname === "/sky" ? " bg-blue-800" : ""}`}>SKY</div>
     <div onClick={() => navigate("/sign-up")} 
-    className=" cursor-pointer">REGISTER</div>
+    className="flex cursor-pointer">REGISTER</div>
   </div>
   )
 }
