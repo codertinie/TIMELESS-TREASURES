ee9f775c086cf32c34b653cc2fb6e6229fc8f210
codertinie <martin.munyao@moringaschool.com>
2022-10-09 09:28:01 +0300

    Added pictures to the Sky component

--- src/PICASPP/Screens/Sky/index.js
+++ src/PICASPP/Screens/Sky/index.js
@@ -1,5 +1,26 @@
+import { useEffect, useState } from "react";
+
 const Sky=()=>{
-    return <div> Hey I am the Sky Page</div>
+  const [pics, setPics] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:8001/pictures")
+    .then (resp => resp.json())
+    .then(setPics)
+  },[])
+
+  const skyPics = pics.filter(obj => {
+    return obj.category === "sky";
+  })
+  return (
+    <div className=" flex flex-row flex-wrap gap-x-4 gap-y-3 place-content-center">
+    {skyPics.map((pic, id) => {
+      return <div key={id} className=" rounded-lg flex flex-col shadow-lg">
+        <img className="w-96" src={pic.url} />
+      </div>
+    })}
+  </div>
+  )
   
   };
   
